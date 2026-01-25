490f85a3153ce439acd7601fe6fc8371c2c0b84b
anjali <anjaliyermalkumar@gmail.com>
2026-01-25 15:21:23 -0500

    added code to read latest pipeline of photon camera and log results from best target

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -9,19 +9,26 @@ import com.pathplanner.lib.path.PathPlannerPath;
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
 import edu.wpi.first.math.geometry.*;
+import edu.wpi.first.util.sendable.Sendable;
 import edu.wpi.first.wpilibj.*;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.container.MiniBotContainer;
 import frc.robot.container.RobotContainer;
+import org.littletonrobotics.junction.LogTable;
 import org.littletonrobotics.junction.LoggedRobot;
 import org.littletonrobotics.junction.Logger;
 import org.photonvision.EstimatedRobotPose;
 import org.photonvision.PhotonCamera;
 import org.photonvision.PhotonPoseEstimator;
+import org.photonvision.PhotonUtils;
+import org.photonvision.targeting.PhotonPipelineResult;
 import org.photonvision.targeting.PhotonTrackedTarget;
 
+import java.io.IOException;
+import java.util.List;
 import java.util.Optional;
 import java.util.function.BooleanSupplier;
 
@@ -33,9 +40,20 @@ public class Robot extends LoggedRobot {
     public RobotContainer robot = new MiniBotContainer();
     public Joystick driveJoystick = new Joystick(0);
     private CommandScheduler scheduler;
-    public static final AprilTagFieldLayout kTagLayout =
-            AprilTagFieldLayout.loadField(AprilTagFields.kDefaultField);
+
+    public static final AprilTagFieldLayout kTagLayout;
+
+    static {
+        try {
+            kTagLayout = new AprilTagFieldLayout("2026-rebuilt-welded.json");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     PhotonCamera photonCamera = new PhotonCamera("PhotonCamera1");
+
+    List<PhotonPipelineResult> result;
     public static final Transform3d kRobotToCam =
             new Transform3d(new Translation3d(0.0, 0.0, 0.0), new Rotation3d(0, 0, 0));
     PhotonPoseEstimator photonEstimator = new PhotonPoseEstimator(kTagLayout, kRobotToCam);
@@ -61,7 +79,33 @@ public class Robot extends LoggedRobot {
     public void robotPeriodic() {
         MOELogger.log();
         scheduler.run();
-/**
+
+        result = photonCamera.getAllUnreadResults();
+        if (!result.isEmpty()) {
+            PhotonPipelineResult latestResult = result.get(result.size() - 1);
+            Logger.recordOutput("Has Targets", latestResult.hasTargets());
+            if (latestResult.hasTargets()) {
+                List<PhotonTrackedTarget> targets = latestResult.getTargets();
+                PhotonTrackedTarget bestTarget = latestResult.getBestTarget();
+                Logger.recordOutput("Target Area", targets.get(0).getArea());
+                Logger.recordOutput("Best Target Area", bestTarget.getArea());
+                Logger.recordOutput("Best Target Yaw", bestTarget.getYaw());
+                Logger.recordOutput("Best Target Pitch", bestTarget.getPitch());
+                Logger.recordOutput("Best Target Fiducial Id", bestTarget.getFiducialId());
+
+
+                Pose3d robotPose = new Pose3d();
+
+                if (kTagLayout.getTagPose(bestTarget.getFiducialId()).isPresent()) {
+                    robotPose = PhotonUtils.estimateFieldToRobotAprilTag(
+                            bestTarget.getBestCameraToTarget(),
+                            kTagLayout.getTagPose(bestTarget.getFiducialId()).get(),
+                            kRobotToCam);
+                }
+                Logger.recordOutput("Robot Pose", robotPose);
+            }
+
+        /*
         Optional<EstimatedRobotPose> visionEst = Optional.empty();
         for (var result : photonCamera.getAllUnreadResults()) {
             visionEst = photonEstimator.estimateCoprocMultiTagPose(result);
@@ -89,9 +133,13 @@ public class Robot extends LoggedRobot {
                         estConsumer.accept(est.estimatedPose.toPose2d(), est.timestampSeconds, estStdDevs);
                     });
         }
-**/
-       // boolean connected = photonCamera.isConnected();
-        // Logger.recordOutput("Camera Connected", connected);
+
+
+         */
+            // boolean connected = photonCamera.isConnected();
+            // Logger.recordOutput("Camera Connected", connected);
+        }
+
     }
 
 
--- src/main/java/frc/robot/subsystem/PhotonCameraSubsystem.java
+++ src/main/java/frc/robot/subsystem/PhotonCameraSubsystem.java
@@ -5,13 +5,13 @@ import edu.wpi.first.wpilibj2.command.Subsystem;
 import org.littletonrobotics.junction.AutoLog;
 import org.littletonrobotics.junction.inputs.LoggableInputs;
 import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonPoseEstimator;
 
 public interface PhotonCameraSubsystem extends Subsystem, LoggableInputs {
     // Change this to match the name of your camera
 
     @AutoLog
     class CameraInputs {
-
     }
 
 }
