2d792fb377843e70c75d7f0dfb16dc62b4907950
anjali <anjaliyermalkumar@gmail.com>
2026-02-05 19:06:46 -0500

    new untested code

--- src/main/java/frc/robot/commands/FaceTargetCommand.java
+++ src/main/java/frc/robot/commands/FaceTargetCommand.java
@@ -5,7 +5,6 @@ import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.units.Units;
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.command.Command;
 import frc.robot.subsystem.CameraControl;
 import frc.robot.subsystem.TankDrive;
 import org.littletonrobotics.junction.Logger;
