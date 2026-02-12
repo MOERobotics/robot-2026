f2e94b540d5e520693d27ed03f26dde9ba8688cd
anjali <anjaliyermalkumar@gmail.com>
2026-02-12 18:24:59 -0500

    worked on face target command and added methods to camera control

--- src/main/java/frc/robot/subsystem/CameraControl.java
+++ src/main/java/frc/robot/subsystem/CameraControl.java
@@ -17,6 +17,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+
+
 public class CameraControl extends MOESubsystem<VisionInputsAutoLogged> implements PhotonCameraSubsystem {
 
 
