a66bc0ab7af27fb502f51e283110f94fda5f7d95
TEST 1 <ellingtonc700@gmail.com>
2026-02-12 18:58:33 -0500

    RENAME and container pid name corrected

--- src/main/java/frc/robot/Interfaces/CollectorSubsystem.java
+++ src/main/java/frc/robot/Interfaces/CollectorSubsystem.java
@@ -2,17 +2,41 @@ package frc.robot.Interfaces;
 
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.Velocity;
+import edu.wpi.first.units.measure.Voltage;
+import org.littletonrobotics.junction.AutoLog;
+
+import static edu.wpi.first.units.Units.*;
 
 public interface CollectorSubsystem {
-    class
+    @AutoLog
+    class AlgaeCollectorInputs {
+        public AngularVelocity wheelVelocity = RPM.zero();
+        public Angle collectorArmAngle = Degrees.zero();
+        public boolean inStartPosition;
+        public boolean inCollectPosition;
+        public AngularVelocity collectorArmVelocity = RadiansPerSecond.zero();
+
+    }
 
     CollectorSubsystem getSensors();
 
-    default void setArmVelocity(AngularVelocity armVelocity) {
+    default void setArmVelocity(AngularVelocity ArmVelocity) {
     }
 
     default void setWheelVelocity(AngularVelocity wheelVelocity) {
     }
 
+    default Angle getArmAngle() {
+        return this.getSensors().getArmAngle();
+    }
+
+    default boolean inStartPosition() {
+        return this.getSensors().inStartPosition();
+    }
+
+    default boolean inCollectPosition() {
+        return this.getSensors().inCollectPosition();
+    }
 
 }
