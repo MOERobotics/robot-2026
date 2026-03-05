e57a4ad65d88ebcfd5df692f387bdd4afbe39285
TEST 1 <ellingtonc700@gmail.com>
2026-03-04 19:52:58 -0500

    for zac

--- src/main/java/frc/robot/container/ProMOEtheus.java
+++ src/main/java/frc/robot/container/ProMOEtheus.java
@@ -117,6 +117,8 @@ public class ProMOEtheus extends RobotContainer {
         SparkMax collectorArmMotor = new SparkMax(15, SparkLowLevel.MotorType.kBrushless); // not confrimed arm id
         SparkMax collectorRollerMotor= new SparkMax(17, SparkLowLevel.MotorType.kBrushless); // not confrimed arm id
         collectorRollerMotor.setInverted(true);
+        collectorArmMotor.setInverted(true);
+
         Angle collectorArmBottom = Degrees.of(137);
         Angle collectorArmTop = Degrees.of(223);
 
