d3c3182e05a34e56a09af458e25b861d375f2c76
ZachMourtada <zach.mourtada@gmail.com>
2026-03-30 15:51:45 -0400

    better autos for neutral zone (completed legs)

--- src/main/java/frc/robot/commands/autos/NeutralZone.java
+++ src/main/java/frc/robot/commands/autos/NeutralZone.java
@@ -18,18 +18,18 @@ public class NeutralZone {
 
     //TODO: make a LA-ROTATION path
     public static Autos.CommandAndPose NLT(RobotContainer robot) {
-        return buildNeutral(robot, "NLT-Neutral", "LN Collect", "RN Return", "RN Return-2", "RA-Rotation");
+        return buildNeutral(robot, "NLT-Neutral", "LN Collect", "RN Return-1", "LN Return-2", "LA-Rotation", "L BackToN");
     }
 
     public static Autos.CommandAndPose NRT(RobotContainer robot) {
-        return buildNeutral(robot, "NRT-Neutral", "RN Collect", "LN Return", "RN Return-2", "RA-Rotation");
+        return buildNeutral(robot, "NRT-Neutral", "RN Collect", "LN Return-1", "LN Return-2", "LA-Rotation", "L BackToN");
     }
 
     public static Autos.CommandAndPose NRT_Back(RobotContainer robot) {
-        return buildNeutral(robot, "NRT-Neutral", "RN Collect", "RN Return-1", "RN Return-2", "RA-Rotation");
+        return buildNeutral(robot, "NRT-Neutral", "RN Collect", "RN Return-1", "RN Return-2", "RA-Rotation", "R BackToN");
     }
     public static Autos.CommandAndPose ART_Back(RobotContainer robot) {
-        return buildNeutral(robot, "ART-Neutral", "RN Collect", "RN Return-1","RN Return-2", "RA-Rotation");
+        return buildNeutral(robot, "ART-Neutral", "RN Collect", "RN Return-1","RN Return-2", "RA-Rotation", "R BackToN");
     }
     public static Autos.CommandAndPose buildNeutral(
             RobotContainer robot,
@@ -37,13 +37,15 @@ public class NeutralZone {
             String path2,
             String path3,
             String path4,
-            String path5) {
+            String path5,
+            String path6) {
 
         PathsFollower plannerPath1 = new PathsFollower(path1);
         PathsFollower plannerPath2 = new PathsFollower(path2);
         PathsFollower plannerPath3 = new PathsFollower(path3);
         PathsFollower plannerPath4 = new PathsFollower(path4);
         PathsFollower plannerPath5 = new PathsFollower(path5);
+        PathsFollower plannerPath6 = new PathsFollower(path6);
 
         Pose2d startingPose = plannerPath1.path.getStartingHolonomicPose().get();
 
@@ -52,24 +54,23 @@ public class NeutralZone {
 
                 new FuelCollectorAutoCommand(robot, true, true, "stop").withTimeout(1),
                 Commands.deadline(
-                        plannerPath1),
-              //          new FuelCollectorAutoCommand(robot ,true, true, "in")),
+                        Commands.sequence(
+                                plannerPath1,
+                                Commands.runOnce(() -> robot.getRobotSwerveDrive().stop()),
+                                plannerPath2,
+                                Commands.runOnce(() -> robot.getRobotSwerveDrive().stop()),
 
-                Commands.runOnce(() -> robot.getRobotSwerveDrive().stop()),
-                Commands.deadline(
-                        plannerPath2),
-                        //new FuelCollectorAutoCommand(robot ,true, true, "in")),
-                Commands.runOnce(() -> robot.getRobotSwerveDrive().stop()),
-               // new FuelCollectorAutoCommand(robot ,true, true, "stop").withTimeout(1),
+                                ),
+                        new FuelCollectorAutoCommand(robot ,true, true, "in")),
                 plannerPath3,
-
                 Commands.runOnce(() -> robot.getRobotSwerveDrive().stop()),
                 plannerPath4,
-
                 Commands.runOnce(() -> robot.getRobotSwerveDrive().stop()),
                 plannerPath5,
+                Commands.runOnce(() -> robot.getRobotSwerveDrive().stop()),
+                // TODO ADD SHOOT
+                plannerPath6,
                 Commands.runOnce(() -> robot.getRobotSwerveDrive().stop())
-                //new ShooterAutoCommand(robot)
         );
 
 
