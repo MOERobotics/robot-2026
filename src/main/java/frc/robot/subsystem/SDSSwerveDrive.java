6a84f7b5f8cd241a2e74ec8d25860f2241e130c6
ZachMourtada <zach.mourtada@gmail.com>
2026-03-31 21:03:19 -0400

    we are now actually using cameras :D

--- src/main/java/frc/robot/subsystem/SDSSwerveDrive.java
+++ src/main/java/frc/robot/subsystem/SDSSwerveDrive.java
@@ -132,14 +132,18 @@ public class SDSSwerveDrive extends MOESubsystem<SwerveDriveInputsAutoLogged> im
         sensors.modulePositions = Arrays.stream(swerveModules).map(SwerveModuleSubsystem::getTravelDistanceNRobotAngle).toArray(SwerveModulePosition[]::new);
         sensors.moduleStates = Arrays.stream(swerveModules).map(SwerveModuleSubsystem::getSpeedNDirectionOfMod).toArray(SwerveModuleState[]::new);
         boolean rejectUpdate = false;
+        photonPoses();
 
-        if(getSensors().photon1 == null || getSensors().photon2 == null){
+        if(getSensors().photon1 == null){
+            rejectUpdate = true;
+        }
+        if (getSensors().photon2 == null){
             rejectUpdate = true;
         }
         if(robotGyro.getAngularVelocityZWorld().getValue().abs(DegreesPerSecond)>=360){
             rejectUpdate = true;
         }
-        //if(getSensors().photon1.){}
+
         Logger.recordOutput("rejectUpdate", rejectUpdate);
 
         if(!rejectUpdate){
