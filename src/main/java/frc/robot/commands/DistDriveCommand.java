37e96f8e608bb7843d93ffde5d44d31c1c0790e7
ZachMourtada <zach.mourtada@gmail.com>
2026-04-15 21:46:14 -0400

    fixed i hope ;-;

--- src/main/java/frc/robot/commands/DistDriveCommand.java
+++ src/main/java/frc/robot/commands/DistDriveCommand.java
@@ -19,6 +19,7 @@ public class DistDriveCommand extends Command {
     ChassisSpeeds setChassisSpeeds;
     Translation2d initTranslation;
     Translation2d currTranslation;
+    SwerveModuleState[] defaultState;
 
 
     public DistDriveCommand (RobotContainer robot, Distance targetDist, ChassisSpeeds setChassisSpeeds){
@@ -31,20 +32,17 @@ public class DistDriveCommand extends Command {
     public void initialize() {
         currDist = Meters.zero();
         initTranslation = robot.getRobotSwerveDrive().getPose().getTranslation();
-        robot.getRobotSwerveDrive().robotDrive(setChassisSpeeds,true);
-        SwerveModuleState[] defaultState = new SwerveModuleState[4];
-        defaultState[0] = new SwerveModuleState(0, Rotation2d.k180deg);
-        defaultState[1] = new SwerveModuleState(0, Rotation2d.k180deg);
-        defaultState[2] = new SwerveModuleState(0, Rotation2d.k180deg);
-        defaultState[3] = new SwerveModuleState(0, Rotation2d.k180deg);
 
-        robot.getRobotSwerveDrive().setModuleStates(defaultState);
+        //robot.getRobotSwerveDrive().setModuleStates(defaultState);
     }
 
     @Override
     public void execute() {
+
+        robot.getRobotSwerveDrive().robotDrive(setChassisSpeeds, true);
         currTranslation = robot.getRobotSwerveDrive().getPose().getTranslation();
         currDist = Meters.of(initTranslation.getDistance(currTranslation));
+
         Logger.recordOutput("DistDriveCommandStuff/targetDist", targetDist);
         Logger.recordOutput("DistDriveCommandStuff/currDist", currDist);
         Logger.recordOutput("DistDriveCommandStuff/currTranslation", currTranslation);
@@ -60,5 +58,14 @@ public class DistDriveCommand extends Command {
     @Override
     public boolean isFinished() {
         return currDist.gte(targetDist);
+    }
+
+    public void rotateModules (){
+        for (int i = 0; i < defaultState.length; i++) {
+            SwerveModuleState moduleState = defaultState[i];
+            //moduleState.optimize(swerveModules[i].getTravelDistanceNRobotAngle().angle);
+
+            robot.getRobotSwerveDrive().getModules()[i].setPivot(moduleState.angle);
+        }
     }
 }
