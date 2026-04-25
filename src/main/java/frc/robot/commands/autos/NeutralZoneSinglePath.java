4fda4643747f2b12c459d44f4d66c787e8ac0365
ZachMourtada <zach.mourtada@gmail.com>
2026-04-25 14:52:38 -0400

    new auto megapaths

---
+++ src/main/java/frc/robot/commands/autos/NeutralZoneSinglePath.java
@@ -0,0 +1,71 @@
+package frc.robot.commands.autos;
+
+
+import com.pathplanner.lib.events.EventTrigger;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.Autos;
+import frc.robot.commands.FuelCollectorAutoCommand;
+import frc.robot.commands.PathsFollower;
+import frc.robot.commands.ShooterAutoAimCommand;
+import frc.robot.container.RobotContainer;
+
+import java.util.HashMap;
+
+
+public class NeutralZoneSinglePath {
+
+    public static Autos.CommandAndPose NeutralMegaPathRight (RobotContainer robot) {
+        return buildNeutral(robot, "Right Neutral 1");
+    }
+    public static Autos.CommandAndPose NeutralMegaPathLeft (RobotContainer robot) {
+        return buildNeutral(robot, "Left Neutral 1");
+    }
+
+    public static Autos.CommandAndPose buildNeutral(
+            RobotContainer robot,
+            String path1) {
+
+        HashMap<String, Command> triggerMap = new HashMap<>();
+
+        PathsFollower plannerPath1 = new PathsFollower(path1);
+
+        Pose2d startingPose = plannerPath1.path.getStartingHolonomicPose().get();
+
+        //triggerMap.put("Run Rollers", new FuelCollectorAutoCommand(robot, true, true, "in"))
+
+
+        Command auto = Commands.sequence(
+
+
+
+                new FuelCollectorAutoCommand(robot, true, true, "stop").withTimeout(0.5),
+                Commands.deadline(
+                plannerPath1,
+                /*
+                new EventTrigger("Run Rollers").whileTrue(
+                        new FuelCollectorAutoCommand(
+                                robot,
+                                true,
+                                true,
+                                "in")).onFalse(
+                                        new FuelCollectorAutoCommand(
+                                                robot,
+                                                true,
+                                                true,
+                                                "stop")),
+
+                 */
+                new FuelCollectorAutoCommand(robot ,true, true, "in")),
+                Commands.runOnce(() -> robot.getRobotSwerveDrive().stop()),
+                // TODO ADD SHOOT
+                new ShooterAutoAimCommand(robot)
+        );
+
+
+
+
+        return new Autos.CommandAndPose(auto.withName("Neutral"+path1), startingPose);    }
+
+}
